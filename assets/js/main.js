26989f7b2d523985743506239bf54378cf99fc6a
Artjoms Bugajenkovs <k3nnel123@gmail.com>
2023-12-19 12:17:26 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// adding class
+
+window.addEventListener('scroll', function () {
+    document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135)
+});
+
+
+// owl carousel
+
+$(document).ready(function(){
+    $(".owl-carousel-full").owlCarousel(
+        {   
+            margin:20,
+            responsive:{
+                0:{
+                    items:1
+                },
+
+                350:{
+                    items:2
+                },
+
+                700:{
+                    items:3
+                },
+
+                1000:{
+                    items:4
+                }
+            }
+        }
+    );
+  });
