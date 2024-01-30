844002dca8ffa08be70cc193bf70d0cb6545669a
Artjoms Bugajenkovs <k3nnel123@gmail.com>
2024-01-30 18:18:50 +0200

    updated styles and index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,45 @@
 // adding class
 
-window.addEventListener('scroll', function () {
-    document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135)
+window.addEventListener("scroll", function () {
+  document
+    .getElementById("header-nav")
+    .classList.toggle("headernav-scroll", window.scrollY > 135);
 });
-
 
 // owl carousel
 
-$(document).ready(function(){
-    $(".owl-carousel-full").owlCarousel(
-        {   
-            margin:20,
-            responsive:{
-                0:{
-                    items:1
-                },
-
-                350:{
-                    items:2
-                },
-
-                700:{
-                    items:3
-                },
-
-                1000:{
-                    items:4
-                }
-            }
-        }
-    );
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $("#top").fadeIn();
+    } else {
+      $("#top").fadeOut();
+    }
   });
+
+  $("#top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 100);
+    return false;
+  });
+
+  $(".owl-carousel-full").owlCarousel({
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1,
+      },
+
+      350: {
+        items: 2,
+      },
+
+      700: {
+        items: 3,
+      },
+
+      1000: {
+        items: 4,
+      },
+    },
+  });
+});
