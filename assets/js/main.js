b85f984d6ac146487e5fcf00bfc21f9d015cde21
Artjoms Bugajenkovs <k3nnel123@gmail.com>
2024-02-06 15:07:51 +0200

    created login and register pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+// validation 
+
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
 // scroll
 
 window.addEventListener("scroll", function () {
