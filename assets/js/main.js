6b43a7e6436cb5e6dd90bbaa6cd5550624ec7d13
Artjoms Bugajenkovs <k3nnel123@gmail.com>
2024-02-09 07:54:46 +0200

    added cookies

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,3 +85,29 @@ $(document).ready(function () {
     },
   });
 });
+
+// cookies 
+
+const cookieBox = document.querySelector(".cookies"),
+  buttons = document.querySelectorAll(".button");
+
+  const executeCodes = () => {
+    //if cookie contains beegood-b it will be returned and below of this code will not run
+    if(document.cookie.includes("beegood-b")) return;
+    cookieBox.classList.add("show");
+
+    buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        cookieBox.classList.remove("show");
+
+        //if button was accepted
+        if (button.id == "acceptBtn") {
+          //set cookies for 1 month.
+          document.cookie = "cookieBy= beegood-b; max-age=" + 60 * 60 * 24 * 30;
+        }
+      });
+    });
+  };
+
+  //executeCodes function will be called on webpage load
+  window.addEventListener("load", executeCodes);
