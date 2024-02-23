a179f96d50ae3b7a93d7076e93eb8ec36f908d28
Artjoms Bugajenkovs <k3nnel123@gmail.com>
2024-02-23 11:12:57 +0200

    button anim

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 // validation
 
 (() => {
@@ -54,9 +53,9 @@ document.querySelectorAll("closecart").forEach((item) => {
 $(document).ready(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
-      $("#top").fadeIn();
+      $("#top").fadeIn(900);
     } else {
-      $("#top").fadeOut();
+      $("#top").fadeOut(900);
     }
   });
 
@@ -86,28 +85,28 @@ $(document).ready(function () {
   });
 });
 
-// cookies 
+// cookies
 
 const cookieBox = document.querySelector(".cookies"),
   buttons = document.querySelectorAll(".button");
 
-  const executeCodes = () => {
-    //if cookie contains beegood-b it will be returned and below of this code will not run
-    if(document.cookie.includes("beegood-b")) return;
-    cookieBox.classList.add("show");
+const executeCodes = () => {
+  //if cookie contains beegood-b it will be returned and below of this code will not run
+  if (document.cookie.includes("beegood-b")) return;
+  cookieBox.classList.add("show");
 
-    buttons.forEach(button => {
-      button.addEventListener("click", () => {
-        cookieBox.classList.remove("show");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
 
-        //if button was accepted
-        if (button.id == "acceptBtn") {
-          //set cookies for 1 month.
-          document.cookie = "cookieBy= beegood-b; max-age=" + 60 * 60 * 24 * 30;
-        }
-      });
+      //if button was accepted
+      if (button.id == "acceptBtn") {
+        //set cookies for 1 month.
+        document.cookie = "cookieBy= beegood-b; max-age=" + 60 * 60 * 24 * 30;
+      }
     });
-  };
+  });
+};
 
-  //executeCodes function will be called on webpage load
-  window.addEventListener("load", executeCodes);
+//executeCodes function will be called on webpage load
+window.addEventListener("load", executeCodes);
